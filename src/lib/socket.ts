beeedcd8726f866747638ff770dbe3c08c8af202
Panudet ingai <codedroy3@gmail.com>
2025-10-16 17:10:11 +0700

    feat: enhance socket integration and notification system across components

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,12 +1,21 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
-export default function SocketClient() {
+export default function SocketServer(userId: string): Socket {
   if (!process.env.NEXT_PUBLIC_SOCKET_IO_URL) {
     throw new Error("NEXT_PUBLIC_SOCKET_IO_URL is not defined");
   }
 
-  const socket = io(process.env.NEXT_PUBLIC_SOCKET_IO_URL!, {
-    withCredentials: false,
+  const socket = io(process.env.NEXT_PUBLIC_SOCKET_IO_URL, {
+    transports: ["websocket"],
+    reconnectionDelayMax: 10000,
+    path: "/socket.io/",
+    auth: { userId },
+    query: {
+      "userId": userId  // ถ้าใช้ auth แล้ว อาจไม่ต้องใช้ query ก็ได้
+    }
+  });
+  socket.on("connect", () => {
+    console.log("Connected to socket server with ID:", socket.id);
   });
   return socket;
 }
