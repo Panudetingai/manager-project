debe90d8cd9bef534b11bdad36529fc6b3ab1153
Panudetingai <codedroy3@gmail.com>
2025-11-11 11:23:39 +0700

    feat: Update dependencies and configuration; add babel-plugin-react-compiler, upgrade Next.js to v16, and enhance TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
  images: {
   domains: ["lh3.googleusercontent.com", "plus.unsplash.com"]
  },
- devIndicators: false
+ devIndicators: false,
+ experimental: {
+    turbopackFileSystemCacheForDev: true,
+ }
 };
 
 export default nextConfig;
